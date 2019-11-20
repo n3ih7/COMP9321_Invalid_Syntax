446bc8ac5eee8aa656b44dfbd4f8841ff23e7bae
Miko <49465216+kanadeqd@users.noreply.github.com>
2019-11-20 22:32:59 +1100

    ui

--- 9321AS/webUI_new/function.js
+++ 9321AS/webUI_new/function.js
@@ -1,12 +1,14 @@
 
-    document.getElementById("button").addEventListener("click",function(){
+    document.getElementById("b1").addEventListener("click",function(){
 
       let city_name = document.getElementById("city").value
-      console.log('http://vau.nono.fi/todo/api/v1.0/weather?cityName='+city_name)
+     
+      console.log('http://vau.nono.fi/todo/api/v1.0/weather_current?cityName='+city_name)
         $.ajax({
-            url:'http://vau.nono.fi/todo/api/v1.0/weather?cityName=' + city_name,
+            url:'http://vau.nono.fi/todo/api/v1.0/weather_current?cityName=' + city_name,
             type:'get',
             success:function(response){
+                console.log(response)
                 console.log(response.text);   //jquery框架会自动处理json，直接用key获取值就可以了
             }
         });
@@ -14,3 +16,4 @@
 
 
     })
+
